192d4aaf439a348555155d6f23df8650b3100a45
Юрий Гринёв <a1d516ac5f5d290@gmail.com>
2020-10-06 00:05:03 +0500

    deploy

---
+++ precache-manifest.530febe6600496f6da16b672488e61ac.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ab9ea2103a0d4cfedb4",
+    "url": "/css/app.481feedc.css"
+  },
+  {
+    "revision": "1f85bf74ca7b897c1d11",
+    "url": "/css/chunk-1760406a.b2478a00.css"
+  },
+  {
+    "revision": "a74103f98038ce74bee1e335a41f6fbc",
+    "url": "/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "4c4fe72d9f2a1ba00140ae463435966b",
+    "url": "/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a547ba68ef3d205712815e81e4421e80",
+    "url": "/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "b3998627e77d9f023d9a246f8b1b3641",
+    "url": "/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "a70297991442a9a459aea1b11357da07",
+    "url": "/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "af9ea31477bd9434cc43f1ee5b306b44",
+    "url": "/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "f22d501a35a87d9f21701cb031f6ea17",
+    "url": "/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "b9aa277fcfc34c31db6c7a7ea3469b8c",
+    "url": "/icons/site.webmanifest"
+  },
+  {
+    "revision": "15c2a07ea99d46a7e77f1eaf09a59db0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ab9ea2103a0d4cfedb4",
+    "url": "/js/app.67fafa99.js"
+  },
+  {
+    "revision": "1f85bf74ca7b897c1d11",
+    "url": "/js/chunk-1760406a.9f1e2e7d.js"
+  },
+  {
+    "revision": "b6a756578b6072105853",
+    "url": "/js/chunk-vendors.ba170d31.js"
+  },
+  {
+    "revision": "c4a5b2a5fd7512005bebca0fce940ed5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
